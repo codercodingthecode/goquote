713fe10ce446fc6d41cabaeb6ee77cc5692961c3
codercodingthecode <andersonlavor@gmail.com>
2018-07-31 00:31:15 -0400

    clean up on main:

--- daixquote.go
+++ daixquote.go
@@ -14,20 +14,4 @@ func main() {
 
 	fmt.Println("THERE WAS AN ERROR ---> ", <-chStatus)
 
-	// ch := make(chan interface{})
-	// chOut := make(chan interface{})
-	// cL := currencyfetch.FetchCurrencies()
-
-	// fmt.Println(cL, ch)
-	// for exchange, pairs := range cL {
-	// 	fmt.Println("RUNNING ON MAIN")
-	// 	// dispatch a go routine per exchange on the list
-	// 	go socketsclient.SocketStream(exchange, pairs, ch, currencyfetch.ExchangeString)
-	// }
-
-	// for {
-	// 	// fmt.Println(<-ch)
-	// }
-	// exchanges.Gdax(ch)
-
 }
